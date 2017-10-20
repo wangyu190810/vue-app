e8c19f052403eeafb14ab7feb0e161463a76de3a
wangyu <190810401@qq.com>
2017-10-20 23:36:51 +0800

    初始化项目

---
+++ 22too/src/components/bloglist.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="bloglist">
+    <h1>22too blog</h1>
+    <a> written by {{ author }} </a>
+    <div v-for="article in articles" class="text item">
+        {{article.title}}
+      </div>   
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  data() {
+    return {
+      author: "22too",
+      articles: []
+    };
+  }
+};
+</script>
+
+<style>
+
+</style>
+
+
