b4daa795b824783496b91d9f03813bafda6eebe8
wangyu <190810401@qq.com>
2017-10-21 15:56:54 +0800

    添加服务端接口访问

--- 22too/src/components/bloglist.vue
+++ 22too/src/components/bloglist.vue
@@ -2,19 +2,50 @@
   <div id="bloglist">
     <h1>22too blog</h1>
     <a> written by {{ author }} </a>
-    <div v-for="article in articles" class="text item">
-        {{article.title}}
-      </div>   
+    <div v-for="blog in blog_lists">
+ 
+      <p>
+        {{blog.title}}
+        {{blog.date}}
+      <div  v-html="blog.content_html">
+      </div>
+      </p>
+     </div>
+  
   </div>
 </template>
 
 <script type="text/javascript">
+import config from "../config"
 export default {
   data() {
     return {
       author: "22too",
-      articles: []
+      articles: [],
+      blog_lists: []
     };
+  },
+  mounted: function() {
+    this.$http
+      .get(
+        config.host+"api/index",
+        {},
+        {
+          headers: {},
+          emulateJSON: true
+        }
+      )
+      .then(
+        function(response) {
+          // 这里是处理正确的回调
+          this.blog_lists = response.body.blogs;
+          // this.articles = response.data["subjects"] 也可以
+        },
+        function(response) {
+          // 这里是处理错误的回调
+          console.log(response);
+        }
+      );
   }
 };
 </script>
---
+++ 22too/src/config.js
@@ -0,0 +1,8 @@
+var config = {
+    // debug 为 true 时，用于本地调试
+    debug: true,
+    host:"http://www.22too.com/"
+    
+    // 其他配置项...
+  };
+  module.exports = config;
